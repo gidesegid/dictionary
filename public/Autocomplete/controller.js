3361e77e3794445dab5efc5826b593040d26ecfc
gidesegid <gidesegid@gmail.com>
2016-10-20 19:59:03 +0200

    autocomplete test

--- public/Autocomplete/controller.js
+++ public/Autocomplete/controller.js
@@ -1,46 +1,59 @@
 var app = angular.module('app', ['autocomplete']);
 
 // the service that retrieves some movie title from an url
-app.factory('dataRetriever', function($http, $q, $timeout){
-  var dataRetriever = new Object();
+app.factory('MovieRetriever', function($http, $q, $timeout){
+  var MovieRetriever = new Object();
 
-  dataRetriever.getdata = function(i) {
-    var data = $q.defer();
-    var words;
-   
-    var moreWords = [];
+  MovieRetriever.getmovies = function(i) {
+    var moviedata = $q.defer();
+    var movies;
+    var data;
+    var test=[]
+var plans;
+   // var someMovies = ["The Wolverine", "The Smurfs 2", "The Mortal Instruments: City of Bones", "Drinking Buddies", "All the Boys Love Mandy Lane", "The Act Of Killing", "Red 2", "Jobs", "Getaway", "Red Obsession", "2 Guns", "The World's End", "Planes", "Paranoia", "The To Do List", "Man of Steel"];
+$http.get('/auto').then(function(data){
 
-    $http.get('/auto').success(function(response){
-      moreWords=response;
-      console.log(moreWords);
-    });
+        plans = JSON.stringify((data.data).map(function(obj){ return obj.languages }));
+       
+        console.log(plans)
+  var moreMovies=plans;     
+//var moreMovies = ["The Wolverine", "The Smurfs 2", "The Mortal Instruments: City of Bones", "Drinking Buddies", "All the Boys Love Mandy Lane", "The Act Of Killing", "Red 2", "Jobs", "Getaway", "Red Obsession", "2 Guns", "The World's End", "Planes", "Paranoia", "The To Do List", "Man of Steel", "The Way Way Back", "Before Midnight", "Only God Forgives", "I Give It a Year", "The Heat", "Pacific Rim", "Pacific Rim", "Kevin Hart: Let Me Explain", "A Hijacking", "Maniac", "After Earth", "The Purge", "Much Ado About Nothing", "Europa Report", "Stuck in Love", "We Steal Secrets: The Story Of Wikileaks", "The Croods", "This Is the End", "The Frozen Ground", "Turbo", "Blackfish", "Frances Ha", "Prince Avalanche", "The Attack", "Grown Ups 2", "White House Down", "Lovelace", "Girl Most Likely", "Parkland", "Passion", "Monsters University", "R.I.P.D.", "Byzantium", "The Conjuring", "The Internship"]
+
+console.log(moreMovies)
+   // if(i && i.indexOf('T')!=-1)
+    //  movies=moreMovies;
+  //  else
+      movies=moreMovies;
 
     $timeout(function(){
-      data.resolve(moreWords);
+      moviedata.resolve(movies);
     },1000);
+})
+//console.log(data);
+    
 
-    return data.promise
+    return moviedata.promise
   }
 
-  return dataRetriever;
+  return MovieRetriever;
 });
 
-app.controller('MyCtrl', function($scope, dataRetriever){
+app.controller('MyCtrl', function($scope, MovieRetriever){
 
-  $scope.words = dataRetriever.getdata("...");
-  $scope.words.then(function(data){
-    $scope.words = data;
+  $scope.movies = MovieRetriever.getmovies("...");
+  $scope.movies.then(function(data){
+    $scope.movies = data;
   });
 
-  $scope.getdata = function(){
-    return $scope.words;
+  $scope.getmovies = function(){
+    return $scope.movies;
   }
 
   $scope.doSomething = function(typedthings){
     console.log("Do something like reload data with this: " + typedthings );
-    $scope.newWords = dataRetriever.getmovies(typedthings);
-    $scope.newWords.then(function(data){
-      $scope.words = data;
+    $scope.newmovies = MovieRetriever.getmovies(typedthings);
+    $scope.newmovies.then(function(data){
+      $scope.movies = data;
     });
   }
 
@@ -49,3 +62,16 @@ app.controller('MyCtrl', function($scope, dataRetriever){
   }
 
 });
+// app.service('myservice',function($http){
+//   var mydata=function(){
+//   $http.get('/auto').success(function(response){
+//     var 
+//   })
+//   }
+// })
+// app.controller('mycontroller',function($scope,$http){
+//   $http.get('/auto').success(function(response){
+//     $scope.data=response;
+//   })
+
+// })
