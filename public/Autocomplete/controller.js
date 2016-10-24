5aca856f2a4b922b0f40ca408fd9a8193bd58a58
gidesegid <gidesegid@gmail.com>
2016-10-24 19:38:39 +0200

    update at index2.html

--- public/Autocomplete/controller.js
+++ public/Autocomplete/controller.js
@@ -4,17 +4,12 @@ var app = angular.module('app', ['autocomplete']);
 //module.value('myprovider',"provider value");
 app.factory('dataRetriever', function($http,$q, $timeout){
   var dataRetriever = new Object();
-
-
- dataRetriever.getdatas = function(toSelectedLanguageId) {
+ dataRetriever.getdatas = function(fromSelectedLanguageId) {
     var data = $q.defer();
     var datas;
     var dataFromRemote;
-    //var x=toSelectedLanguageId;
-
-    console.log("from factory"+toSelectedLanguageId);
-  $http.get('/auto/'+toSelectedLanguageId).then(function(response){
-       
+    console.log("from factory"+fromSelectedLanguageId);
+  $http.get('/auto/'+fromSelectedLanguageId).then(function(response){
        dataFromRemote = JSON.stringify((response.data).map(function(obj){ return obj.text }));
        var datas=JSON.parse(dataFromRemote); 
 
@@ -26,30 +21,18 @@ app.factory('dataRetriever', function($http,$q, $timeout){
 }
   return dataRetriever;
 });
-
 app.controller('MyCtrl',['$scope','$http','dataRetriever', function($scope,$http, dataRetriever){
-$scope.toSelectedLanguageId=2;
-$scope.fromSelectedLanguageId=2;
-$scope.myData=[];
-$scope.datas=[];
-
- var dataPromise = dataRetriever.getdatas($scope.fromSelectedLanguageId);
- dataPromise.then(function(data){
-   $scope.datas = data;
- });
-  //console.log('myprovider'+myprovider);
-
+          $scope.myData=[];
+          $scope.datas=[];
   $scope.getdatas = function(){
     return $scope.datas;
   }
- $http.get('/data').success(function(response){
+  $http.get('/data').success(function(response){
    $scope.myData=response;
-    //console.log("this is from mydata"+response.data)
   });
+
   $scope.doSomething = function(typedthings){
-  
     $http.get('/dataw/'+$scope.fromSelectedLanguageId+'/'+$scope.inputdata+'/'+$scope.toSelectedLanguageId).success(function(response){
-   // console.log(response);
     $scope.outPut=response;
   });
   }
@@ -57,27 +40,11 @@ $scope.datas=[];
   $scope.doSomethingElse = function(suggestion){
     console.log("Suggestion selected: " + suggestion );
   }
-// function getLanguageId($scope,dataRetriever){
-    $scope.toswitchTranslation=function(toSelectedLanguageId){
-    dataRetriever.getdatas($scope.toSelectedLanguageId);
-     console.log("this is from ctlr"+$scope.toSelectedLanguageId)
 
-  } 
-//}
-  
+  $scope.fromSwitchTranslation=function(fromSelectedLanguageId){
+        var dataPromise = dataRetriever.getdatas($scope.fromSelectedLanguageId);
+        dataPromise.then(function(data){
+        $scope.datas = data;
+      });
+    } 
 }]);
-// app.controller('mycontroller',function($scope,$http){
- 
-//   $http.get('/data').success(function(response){
-  
-//     $scope.myData=response;
-//   });
-// $scope.toswitchTranslation=function(){
-
-//   $http.get('/dataw/'+$scope.fromSelectedLanguageId+'/'+$scope.inputdata+'/'+$scope.toSelectedLanguageId).success(function(response){
-//     console.log(response);
-//     $scope.outPut=response;
-//   });
-
-//    }
-//  })
