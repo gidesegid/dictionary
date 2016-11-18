cc86fc5fd2cd25e0b75f4c05e14fef72853dae70
gidesegid <gidesegid@gmail.com>
2016-11-18 16:08:28 +0100

    branch is created for keyboard embeding help

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -22,26 +22,42 @@ app.factory('dataRetriever', function($http,$q, $timeout){
   return dataRetriever;
 });
 
-
 //controller
 app.controller('MyCtrl',['$scope','$http','dataRetriever', function($scope,$http, dataRetriever){
-          $http.get('/languages').success(function(response){
-       $scope.myData=response;
-      });
-
           $scope.myData=[];
           $scope.fromSwitchTranslation=null;
           $scope.toswitchTranslation=null;
   //retrieve languages
-     
+      $http.get('/languages').success(function(response){
+       $scope.myData=response;
+      });
 //filling data from data base to autocomplete textfield
   $scope.fillData = function(typedthings,fromSelectedLanguageId,inputdata){
-     var dataPromise = dataRetriever.getdatas($scope.languages,$scope.inputdata);
-     $scope.datas=null;
+    $scope.fromSelectedLanguageId=fromSelectedLanguageId
+    if($scope.languages===7){
+           
+              $scope.myFunct = function($event){
+             
+                if ($event.which == 113) {
+                    $scope.inputdata +="ቅ";
+                }
+              };
+    }
+   if($scope.languages==null){
+       document.getElementById("remainderLangSelector").innerHTML="Select language from the above.ኣብ ላዕሊ ካብዘለዉ ቛንቛታት ምረጹ"
+       document.getElementById("infoToLang").innerHTML="";
+     }else if($scope.inputdata===""){
+        document.getElementById("infoToLang").innerHTML="";
+     }
+     else{
+      var dataPromise = dataRetriever.getdatas($scope.languages,$scope.inputdata);
+        $scope.datas=null;
         dataPromise.then(function(data){
         $scope.datas = data;
-    });
-  }
+   
+       });
+     }
+   }
 //data selection from autocomplete
   $scope.dataSelection = function(suggestion){
     $scope.datas=null;
@@ -54,191 +70,62 @@ app.controller('MyCtrl',['$scope','$http','dataRetriever', function($scope,$http
   //from languages
   $scope.fromSwitchTranslation=function(fromSelectedLanguageId){
     $scope.languages=fromSelectedLanguageId;
+     document.getElementById("remainderLangSelector").innerHTML="";
+     document.getElementById("infoToLang").innerHTML="";
   } 
-
 //to languages 
     $scope.toswitchTranslation=function(toswitchTranslationId){
-       $http.get('/word2/'+toswitchTranslationId+'/'+$scope.value).success(function(response){
-       $scope.outPut=response;
+      if($scope.inputdata==null){
+        document.getElementById("infoToLang").innerHTML="Please select language from the top and fill word at the input field and then click this button.ካብ ኣብ ላዕሊ ዘለዉ መልጎም ቛንቛ ምረጹ ብድሕሪኡ ኣብ ትሕቲኡ ዘሎ መምልኢ ቦታ ዝደለኹሞ ቃላት ናይቲ ዝመረጽኩሞ ቛንቛ ድሕሪ ምጽሓፍ፣ካብቶም ዝመጽኹም ምርጫታት ውጽኢት ናይቲ ዝጸሓፍኩሞ መረጹ ኣብ መወዳእታ ናብቲ ክትርኮመልኩም ዝደለኹሞ ኣብ ታሕቲ ዘለዉ መልጎም ቛንቛታት ህረሙ።"
+      }else{
+          $http.get('/word2/'+toswitchTranslationId+'/'+$scope.value).success(function(response){
+          $scope.outPut=response;
        });
+      }
     }
 }]);
 
+//    var array = [2, 5, 9];
+// var index = array.indexOf(5);
+//  array.splice(index, 1);
+   
+var buttons = document.getElementsByTagName("button");
+var buttonsCount = buttons.length;
+ var arr=[];
+ var arr2=[1,2,3,4,5,6,7,8,9,10]
+for (var i = 0; i <= buttonsCount; i += 1) {
 
+    buttons[i].onclick = function(e) {
+        document.getElementById(this.id).style.color='red';
+        // arr.shift(this.id);
+        // arr.push(this.id);
+        var ind=this.id
+        console.log("index id "+ind)
+       arr2.splice(ind,-1);
+       // console.log(arr)
+        console.log(arr2)
+        arr2 = arr2.filter(function(item) { 
+          return item == ind;
+         });
+        console.log(arr2); 
+       // console.log("index id of selected item"+ index);
 
+       // for(var j=0;j!==this.id;j+=1){
+       //     document.getElementById(this.id).style.color='black';
+       // }
+    };
+  }
 
-function toEnglish(){
-  document.getElementById("to1").style.color='red';
-  document.getElementById("to2").style.color='black';
-  document.getElementById("to3").style.color='black';
-  document.getElementById("to4").style.color='black';
-  document.getElementById("to5").style.color='black';
-  document.getElementById("to6").style.color='black';
-  document.getElementById("to7").style.color='black';
-  document.getElementById("to8").style.color='black';
+// function toEnglish(){
+//   document.getElementById("to1").style.color='red';
+//   document.getElementById("to2").style.color='black';
+//   document.getElementById("to3").style.color='black';
+//   document.getElementById("to4").style.color='black';
+//   document.getElementById("to5").style.color='black';
+//   document.getElementById("to6").style.color='black';
+//   document.getElementById("to7").style.color='black';
+//   document.getElementById("to8").style.color='black';
+//   document.getElementById("to9").style.color='black';
+//   document.getElementById("to10").style.color='black';
 
-}
-function toDutch(){
-  document.getElementById("to1").style.color='black';
-  document.getElementById("to2").style.color='red';
-  document.getElementById("to3").style.color='black';
-  document.getElementById("to4").style.color='black';
-  document.getElementById("to5").style.color='black';
-  document.getElementById("to6").style.color='black';
-  document.getElementById("to7").style.color='black';
-  document.getElementById("to8").style.color='black';
-
-}
-function toTigrigna(){
-  document.getElementById("to1").style.color='black';
-  document.getElementById("to2").style.color='black';
-  document.getElementById("to3").style.color='black';
-  document.getElementById("to4").style.color='black';
-  document.getElementById("to5").style.color='black';
-  document.getElementById("to6").style.color='black';
-  document.getElementById("to7").style.color='red';
-  document.getElementById("to8").style.color='black';
-
-}
-function toFrench(){
-  document.getElementById("to1").style.color='black';
-  document.getElementById("to2").style.color='black';
-  document.getElementById("to3").style.color='black';
-  document.getElementById("to4").style.color='red';
-  document.getElementById("to5").style.color='black';
-  document.getElementById("to6").style.color='black';
-  document.getElementById("to7").style.color='black';
-  document.getElementById("to8").style.color='black';
-
-}
-function toGerman(){
-  document.getElementById("to1").style.color='black';
-  document.getElementById("to2").style.color='black';
-  document.getElementById("to3").style.color='red';
-  document.getElementById("to4").style.color='black';
-  document.getElementById("to5").style.color='black';
-  document.getElementById("to6").style.color='black';
-  document.getElementById("to7").style.color='black';
-  document.getElementById("to8").style.color='black';
-
-}
-function toItalian(){
-  document.getElementById("to1").style.color='black';
-  document.getElementById("to2").style.color='black';
-  document.getElementById("to3").style.color='black';
-  document.getElementById("to4").style.color='black';
-  document.getElementById("to5").style.color='red';
-  document.getElementById("to6").style.color='black';
-  document.getElementById("to7").style.color='black';
-  document.getElementById("to8").style.color='black';
-}
-function toNorway(){
-  document.getElementById("to1").style.color='black';
-  document.getElementById("to2").style.color='black';
-  document.getElementById("to3").style.color='black';
-  document.getElementById("to4").style.color='black';
-  document.getElementById("to5").style.color='black';
-  document.getElementById("to6").style.color='red';
-  document.getElementById("to7").style.color='black';
-  document.getElementById("to8").style.color='black';
-}
-function toSweeden(){
-  document.getElementById("to1").style.color='black';
-  document.getElementById("to2").style.color='black';
-  document.getElementById("to3").style.color='black';
-  document.getElementById("to4").style.color='black';
-  document.getElementById("to5").style.color='black';
-  document.getElementById("to6").style.color='black';
-  document.getElementById("to7").style.color='black';
-   document.getElementById("to8").style.color='red';
-}
-
-function english(){
-  document.getElementById("1").style.color='red';
-  document.getElementById("2").style.color='black';
-  document.getElementById("3").style.color='black';
-  document.getElementById("4").style.color='black';
-  document.getElementById("5").style.color='black';
-  document.getElementById("6").style.color='black';
-  document.getElementById("7").style.color='black';
-  document.getElementById("8").style.color='black';
-}
-function dutch(){
-  document.getElementById("1").style.color='black';
-  document.getElementById("2").style.color='red';
-  document.getElementById("3").style.color='black';
-  document.getElementById("4").style.color='black';
-  document.getElementById("5").style.color='black';
-  document.getElementById("6").style.color='black';
-  document.getElementById("7").style.color='black';
-   document.getElementById("8").style.color='black';
-}
-function tigrigna(){
-  document.getElementById("1").style.color='black';
-  document.getElementById("2").style.color='black';
-  document.getElementById("3").style.color='black';
-  document.getElementById("4").style.color='black';
-  document.getElementById("5").style.color='black';
-  document.getElementById("6").style.color='black';
-  document.getElementById("7").style.color='red';
-  document.getElementById("8").style.color='black';
-}
-function french(){
-  document.getElementById("1").style.color='black';
-  document.getElementById("2").style.color='black';
-  document.getElementById("3").style.color='black';
-  document.getElementById("4").style.color='red';
-  document.getElementById("5").style.color='black';
-  document.getElementById("6").style.color='black';
-  document.getElementById("7").style.color='black';
-  document.getElementById("8").style.color='black';
-}
-function germen(){
-  document.getElementById("1").style.color='black';
-  document.getElementById("2").style.color='black';
-  document.getElementById("3").style.color='red';
-  document.getElementById("4").style.color='black';
-  document.getElementById("5").style.color='black';
-  document.getElementById("6").style.color='black';
-  document.getElementById("7").style.color='black';
-  document.getElementById("8").style.color='black';
-}
-function italian(){
-  document.getElementById("1").style.color='black';
-  document.getElementById("2").style.color='black';
-  document.getElementById("3").style.color='black';
-  document.getElementById("4").style.color='black';
-  document.getElementById("5").style.color='red';
-  document.getElementById("6").style.color='black';
-  document.getElementById("7").style.color='black';
-   document.getElementById("8").style.color='black';
-}
-function norway(){
-  document.getElementById("1").style.color='black';
-  document.getElementById("2").style.color='black';
-  document.getElementById("3").style.color='black';
-  document.getElementById("4").style.color='black';
-  document.getElementById("5").style.color='black';
-  document.getElementById("6").style.color='red';
-  document.getElementById("7").style.color='black';
-   document.getElementById("8").style.color='black';
-}
-function sweeden(){
-  document.getElementById("1").style.color='black';
-  document.getElementById("2").style.color='black';
-  document.getElementById("3").style.color='black';
-  document.getElementById("4").style.color='black';
-  document.getElementById("5").style.color='black';
-  document.getElementById("6").style.color='black';
-  document.getElementById("7").style.color='black';
-   document.getElementById("8").style.color='red';
-}
-$(document).ready(function(){
-  $('#english').on('click',function(){
-    $('#dutch').hide();
-  })
-  $('.btns').on('click',function(){
-    var panelId=$(this).attr('data-panelid')
-    $('#'+panelId).toggle();
-  })
-})
+// }
--- router/main.js
+++ router/main.js
@@ -35,19 +35,20 @@ module.exports = function(app)
       app.get('/about',function(req,res){
         res.render('about.html');
     });
+       app.get('/login',function(req,res){
+        res.render('login.html');
+    });
        app.get('/paragraph',function(req,res){
         res.render('paragraph.html');
     });
        app.get('/p2',function(req,res){
         res.render('index2.html');
     });
+       //autocomplete data
      app.get('/auto/:languages/:inputdata',function(req,res){
         var id=req.params.languages;
         var inputdata=req.params.inputdata
-       // connection.query("select * from words WHERE languages_id=? LIMIT 20",id,function(error,row,fields){
-     //connection.query("select word_3.word from word_3 where word_3.word='"+inputdata+"' and word_3.language_id='"+id+"'",function(error,row,fields){
-     connection.query("select word_3.word from word_3 where word_3.word like '"+inputdata+"%"+"' and word_3.language_id='"+id+"'",function(error,row,fields){
-      
+        connection.query("select collectedwords.word from collectedwords where collectedwords.word like '"+inputdata+"%"+"' and collectedwords.language_id='"+id+"' LIMIT 10",function(error,row,fields){
         if(!!error){
             console.log(error);
           console.log('error in query')
@@ -58,6 +59,7 @@ module.exports = function(app)
         }
       })
      })
+     //languages data
     app.get('/languages',function(req,res){
   
     connection.query("select * from languages",function(error,row,fields){
@@ -73,10 +75,9 @@ module.exports = function(app)
    });
     //retrieve inputvalue from mysqltable 
     app.get('/word/:fromLanguageId/:suggestion',function(req,res){
-
              var fromLanguageId=req.params.fromLanguageId;
              var inputdata=req.params.suggestion;
-            connection.query("select word_3.wordValueId from word_3 where word_3.word='"+inputdata+"' and word_3.language_id='"+fromLanguageId+"'",function(error,row,fields){
+            connection.query("select collectedwords.wordValueId from collectedwords where collectedwords.word='"+inputdata+"' and collectedwords.language_id='"+fromLanguageId+"'",function(error,row,fields){
                 if(!!error){
                   console.log('error in query')
                 }else{
@@ -86,12 +87,12 @@ module.exports = function(app)
                 }
               });
    });
-
+//output of the translation
     app.get('/word2/:toswitchTranslationId/:datas',function(req,res){
                  var toswitchTranslationId=req.params.toswitchTranslationId;
                  var wordvalue=req.params.datas;
                 
-              connection.query("select word_3.word from word_3 where word_3.wordValueId='"+wordvalue+"' and word_3.language_id='"+toswitchTranslationId+"'",function(error,row,fields){
+              connection.query("select collectedwords.word from collectedwords where collectedwords.wordValueId='"+wordvalue+"' and collectedwords.language_id='"+toswitchTranslationId+"'",function(error,row,fields){
                if(!!error){
                   console.log('error in query')
                 }else{
@@ -101,6 +102,102 @@ module.exports = function(app)
                 }
           
              });
-
     });
+    //retrieve all new words to browther from database
+    app.get('/newWords',function(req,res){
+               
+              connection.query("select * from newwords",function(error,row,fields){
+               if(!!error){
+                  console.log('error in query from database maybee')
+                }else{
+                   console.log('succesfully connected');
+                   console.log(row);
+                   res.send(row);
+                }
+          
+             });
+    });
+    //add new word to database
+    app.post("/addNewWord/:eng/:tig/:id",function(req,res){
+               var eng=req.params.eng;
+               var tig=req.params.tig;
+               var id=req.params.id;
+               console.log(eng)
+               console.log(tig)
+               console.log(id)
+              connection.query("insert into newWords(english,tigrigna,userId) values('"+eng+"'"+","+"'"+tig+"'"+","+"'"+id+"')",function(error,row,fields){
+               if(!!error){
+                  console.log('error in query ፍሮም ዳታባኤዝ')
+                }else{
+                   console.log('succesfully connected');
+                   console.log(row);
+                   res.send(row);
+                }
+             });
+    });
+    //authentication
+    app.get('/login/:userName/:password',function(req,res){
+        var userName=req.params.userName;
+        var password=req.params.password;
+
+            connection.query("select userName,password,id from users where userName=?",userName,function(error,row,fields){
+               if(!!error){
+                  console.log('error in query')
+                }else{
+                   console.log('succesfully connected');
+                   console.log(row);
+                   res.send(row);
+                }
+          
+             });
+    })
+    //retrievd data to updates from table test1
+     app.get('/updates',function(req,res){
+       
+            connection.query("SELECT * from test1 where wordTig='"+"' LIMIT 20",function(error,row,fields){
+               if(!!error){
+                  console.log('error in query')
+                }else{
+                   console.log('succesfully connected');
+                   console.log(row);
+                   res.send(row);
+                }
+          
+             });
+    })
+//update test1 tables
+    app.put('/updateword/:langid/:value/:word/:userId',function(req,res){
+      var id=req.params.langid;
+      var value=req.params.value;
+      var word=req.params.word;
+      var userId=req.params.userId
+     //update to test1 
+     connection.query("UPDATE test1 SET wordTig='"+word+"'"+",userId="+userId+"  WHERE value='"+value+"'",function(error,row,fields){
+       if(!!error){
+          console.log('error in query')
+        }else{
+           console.log('succesfully connected');
+           console.log(row);
+           res.send(row);
+        }
+  
+     });
+
+    })
+//update data in the collectedwords
+     app.put('/updatewordInColl/:langid/:value/:word',function(req,res){
+      var id=req.params.langid;
+      var value=req.params.value;
+      var word=req.params.word;
+      connection.query("UPDATE collectedwords SET word='"+word+"' WHERE wordValueId='"+value+"'"+"AND language_id=7",function(error,row,fields){
+       if(!!error){
+          console.log('error in query')
+        }else{
+           console.log('succesfully connected');
+           console.log(row);
+           res.send(row);
+        }
+  
+     });
+   })  
 }
